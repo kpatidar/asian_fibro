68cf0a1eedebe7f9fe29e145060b97ca52afb5c3
KapilP <kapil9926094723@gmail.com>
2013-04-27 15:07:26 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asian::Application.config.secret_token = 'a1dc8ad7687921f23e61c2f7f460447e569b4ad70b014828dafb8b7f988b1b68ae63a0d0cb042008a1e82f2bb50c44afc9447d5d47b1264c70e1d3e9fdc95f21'
