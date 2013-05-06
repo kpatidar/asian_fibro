afce6c7c1c116a3cddc7046a8de6fabd593eb43a
KapilP <kapil9926094723@gmail.com>
2013-05-06 18:16:50 +0530

    Added contact image

--- app/mailers/order_notifications.rb
+++ app/mailers/order_notifications.rb
@@ -1,6 +1,6 @@
 class OrderNotifications < ActionMailer::Base
   def get_order(order)
     @order = order
-    mail(:to => "mohsinkhan183@gmail.com", :from => "kplguru07@gmail.com", :subject => "Asian Fibro Plast Order from #{@order.customer_fname}")
+    mail(:to => "kplguru07@gmail.com", :from => "kplguru07@gmail.com", :subject => "Asian Fibro Plast Order from #{@order.customer_fname}")
   end
 end
