1d4e7ead54bcba2169f38a5e14062aa7fe4f3eea
KapilP <kapil9926094723@gmail.com>
2013-05-07 15:31:35 +0530

    mailer to setting for dev and prod

--- app/mailers/order_notifications.rb
+++ app/mailers/order_notifications.rb
@@ -1,6 +1,7 @@
 class OrderNotifications < ActionMailer::Base
   def get_order(order)
     @order = order
-    mail(:to => "kplguru07@gmail.com", :from => "kplguru07@gmail.com", :subject => "Asian Fibro Plast Order from #{@order.customer_fname}")
+    to = Rails.env=="production" ? "mohsinkhan183@gmail.com" : "kplguru07@gmail.com"
+    mail(:to => to, :from => "kplguru07@gmail.com", :subject => "Asian Fibro Plast Order from #{@order.customer_fname}")
   end
 end
