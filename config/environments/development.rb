3bbc5c183f9972e6e1ce87657cf57472b0b2f2a4
KapilP <kapil9926094723@gmail.com>
2013-05-07 15:15:39 +0530

    mailer changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,5 +35,16 @@ Asian::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: "localhost" }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "meet071287@gmail.com",
+    password: "meet@2908"
+  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,18 @@ Asian::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: "asianfibroplast.herokuapp.com" }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "meet071287@gmail.com",
+    password: "meet@2908"
+  }
 
   # Enable threaded mode
   # config.threadsafe!
